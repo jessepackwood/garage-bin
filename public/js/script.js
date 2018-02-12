bbc322131f9e1cbee58bb2bd3b8ea58506731f26
Jesse Packwood <Jesse@Jesse.local>
2018-02-12 14:03:53 -0700

    appending items on frontend

--- public/js/script.js
+++ public/js/script.js
@@ -3,12 +3,13 @@ const fetchItems = async () => {
   const unresolvedItems = await fetch('/api/v1/garage_items')
   const fetchedItems = await unresolvedItems.json()
   $('#garage-item-container').empty()
+  // $('#add-item').toggleClass('hidden')
 
   if ($('#garage-door').hasClass('hidden')) {
   fetchedItems.forEach( item => {
     $('#garage-item-container').append(
       `<div>
-        <h3>${item.name}</h3>
+        <h3 class='appended-item-name'>${item.name}</h3>
         <div id='item + ${item.id}' class='hidden item-details'>
           <ul>
             <li>Reason: ${item.reason}</li>
@@ -36,9 +37,12 @@ const fetchItems = async () => {
 // }
 
 const renderItemData = (event) => {
-  if ($(event.target).next().hasClass('item-details')) {
+  // console.log($(event.target).next())
+  // if ($(event.target).next().hasClass('item-details')) {
+  // $(event.target).next().toggleClass('hidden')
+  // }
   $(event.target).next().toggleClass('hidden')
-  }
+
 }
 
 const toggleGarage = () => {
@@ -56,7 +60,6 @@ const addItem = async () => {
   const name = $('#name-input').val()
   const reason = $('#reason-input').val()
   const cleanliness = $('.dropdown').find(':selected').val()
-  console.log(cleanliness)
   try {
   const itemPost = await fetch('/api/v1/garage_items', {
     method: 'POST',
@@ -66,14 +69,33 @@ const addItem = async () => {
     }
   })
   const newItem = await itemPost.json()
-  return newItem
+  console.log(newItem)
+  appendItem()
   } catch (error) {
   }
 }
 
+const appendItem = () => {
+  const name = $('#name-input').val()
+  const reason = $('#reason-input').val()
+  const cleanliness = $('.dropdown').find(':selected').val()
+  $('#garage-item-container').append(
+    `<div>
+        <h3 class='appended-item-name'>${name}</h3>
+        <div class='hidden item-details'>
+          <ul>
+            <li>Reason: ${reason}</li>
+            <li>Cleanliness: ${cleanliness}</li>
+          </ul>
+        </div>
+      </div>`
+    )
+}
+
+
 $('.btn-add').on('click', addItem)
 $('#btn-garage').on('click', openGarage)
-$(document).on('click', renderItemData)
+$(document).on('click', '.appended-item-name', renderItemData)
 
 
 $(document).ready(() => {
