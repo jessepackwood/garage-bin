49f61a7a446451503dc39b96ff3c4f27b0a56f52
Jesse Packwood <Jesse@Jesse.local>
2018-02-12 10:06:22 -0700

    create get and post endpoints

--- server.js
+++ server.js
@@ -17,6 +17,32 @@ app.get('/', (request, response) => {
   response.send('Hello World!');
 });
 
+app.get('/api/v1/garage_items', (request, response) => {
+  database('garage_items').select()
+    .then((items) => {
+      response.status(200).json(items)
+    })
+    .catch((error) => {
+      response.status(422).json({ error })
+    });
+});
+
+app.post('/api/v1/garage_items', (request, response) => {
+  const garageItem = request.body;
+
+  for (const requiredParams of ['name', 'reason', 'cleanliness']) {
+    if (!garageItem[requiredParams]) {
+      return response.status(422).json({
+        error: `You are missing ${requiredParams}`
+      });
+    }
+  }
+
+  database('garage_items').insert(garageItem, 'id')
+    .then(garageItem => response.status(201).json({ id: garageItem[0] }))
+    .catch(error => response.status(404).json({ error }));
+});
+
 app.listen(app.get('port'), () => {
   /* eslint-disable no-console */
   console.log(`${app.locals.title} is running on ${app.get('port')}. env: ${environment}`);
